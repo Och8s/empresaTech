b0f299898ee8e7aa555bfcee595768b9c04f874c
JavierManjónMartos <jmanjonmartos@gmail.com>
2025-04-16 08:22:50 +0200

    Resuelto conflicto de merge

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 
 export default defineConfig({
   base: '/empresaTech/', // Nom del repositori a GitHub
+  build: {
+    outDir: 'docs',  // Canviem la sortida a la carpeta 'docs'
+  },
   plugins: []
 })
